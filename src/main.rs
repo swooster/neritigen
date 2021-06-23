264b3c5f1f04aa56ef7d36ea63d7fa02df3ecae8
Steve Wooster <s.f.m.wooster@gmail.com>
2021-06-23 16:10:41 -0700

    Hello, window!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Hello, window!")
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _event_loop_target, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => {
+                *control_flow = ControlFlow::Exit;
+            }
+            _ => (),
+        }
+    });
 }
