3ae75f146bd5ca4c3e395dd50c8a625d2fbd414c
Steve Wooster <s.f.m.wooster@gmail.com>
2021-06-24 15:01:02 -0700

    Hello, Vulkan!
    This adds a minimal "renderer" that just connects to Vulkan and enables
    logging. I likely won't worry about error-handling until after I have a
    triangle.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::ffi::{CStr, CString};
+use std::os::raw::c_void;
+
+use ash::{
+    extensions::ext::DebugUtils,
+    version::{EntryV1_0, InstanceV1_0},
+    vk,
+};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -5,13 +13,19 @@ use winit::{
 };
 
 fn main() {
+    env_logger::init();
+
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
-        .with_title("Hello, window!")
+        .with_title("Hello, Vulkan!")
         .build(&event_loop)
         .unwrap();
 
+    let renderer = Renderer::new();
+
     event_loop.run(move |event, _event_loop_target, control_flow| {
+        let _renderer = &renderer; // Ensure destructor is run
+
         *control_flow = ControlFlow::Wait;
 
         match event {
@@ -24,4 +38,105 @@ fn main() {
             _ => (),
         }
     });
+}
+
+pub struct Renderer {
+    debug_utils_fn: DebugUtils,
+    debug_utils_messenger: vk::DebugUtilsMessengerEXT,
+    _entry: ash::Entry,
+    instance: ash::Instance,
+}
+
+impl Renderer {
+    pub fn new() -> Self {
+        unsafe {
+            let entry = ash::Entry::new().unwrap();
+            let instance = Self::create_instance(&entry);
+
+            let debug_utils_fn = DebugUtils::new(&entry, &instance);
+            let debug_utils_messenger = debug_utils_fn
+                .create_debug_utils_messenger(&Self::debug_utils_messenger_create_info(), None)
+                .unwrap();
+
+            Self {
+                debug_utils_fn,
+                debug_utils_messenger,
+                _entry: entry,
+                instance,
+            }
+        }
+    }
+
+    unsafe fn create_instance(entry: &ash::Entry) -> ash::Instance {
+        let application_name = CString::new("Nerigen").unwrap();
+        let application_version = vk::make_version(
+            env!("CARGO_PKG_VERSION_MAJOR").parse().unwrap(),
+            env!("CARGO_PKG_VERSION_MINOR").parse().unwrap(),
+            env!("CARGO_PKG_VERSION_PATCH").parse().unwrap(),
+        );
+        let application_info = vk::ApplicationInfo::builder()
+            .application_name(&application_name)
+            .application_version(application_version)
+            .engine_name(&application_name)
+            .engine_version(application_version)
+            .api_version(vk::make_version(1, 0, 0));
+
+        let validation_layer = CString::new("VK_LAYER_KHRONOS_validation").unwrap();
+        let enabled_layer_names = [validation_layer.as_ptr()];
+        let debug_utils = DebugUtils::name();
+        let enabled_extension_names = vec![debug_utils.as_ptr()];
+        let mut debug_utils_messenger_create_info = Self::debug_utils_messenger_create_info();
+        let create_info = vk::InstanceCreateInfo::builder()
+            .application_info(&application_info)
+            .enabled_layer_names(&enabled_layer_names)
+            .enabled_extension_names(&enabled_extension_names)
+            .push_next(&mut debug_utils_messenger_create_info);
+
+        entry.create_instance(&create_info, None).unwrap()
+    }
+
+    fn debug_utils_messenger_create_info() -> vk::DebugUtilsMessengerCreateInfoEXTBuilder<'static> {
+        vk::DebugUtilsMessengerCreateInfoEXT::builder()
+            .message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::all())
+            .message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
+            .pfn_user_callback(Some(Self::debug_utils_callback))
+    }
+
+    unsafe extern "system" fn debug_utils_callback(
+        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+        message_types: vk::DebugUtilsMessageTypeFlagsEXT,
+        p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+        _p_user_data: *mut c_void,
+    ) -> u32 {
+        let message_severity = match message_severity {
+            vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE => log::Level::Debug,
+            vk::DebugUtilsMessageSeverityFlagsEXT::INFO => log::Level::Info,
+            vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => log::Level::Warn,
+            vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => log::Level::Error,
+            _ => log::Level::Error,
+        };
+        let message = CStr::from_ptr((*p_callback_data).p_message);
+        if let Ok(message) = message.to_str() {
+            log::log!(message_severity, "{:?}: {}", message_types, message);
+        } else {
+            log::log!(message_severity, "{:?}: {:?}", message_types, message);
+        }
+        vk::FALSE
+    }
+}
+
+impl Default for Renderer {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Drop for Renderer {
+    fn drop(&mut self) {
+        unsafe {
+            self.debug_utils_fn
+                .destroy_debug_utils_messenger(self.debug_utils_messenger, None);
+            self.instance.destroy_instance(None);
+        }
+    }
 }
